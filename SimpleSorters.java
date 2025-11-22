c4d446805fad0753c6954c57f83d79703cbb5f39
MHUGGLER <murilo.m.huggler@gmail.com>
2025-11-21 21:03:17 -0800

    Add SimpleSorters and SimpleSortDriver with compiled classes

---
+++ SimpleSorters.java
@@ -0,0 +1,52 @@
+import java.util.Comparator;
+
+public class SimpleSorters {
+
+    /**
+     * Optimized Bubble Sort using a swapped flag.
+     */
+    public static <K> void bubbleSort(K[] S, Comparator<K> comp) {
+        int n = S.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+
+            // Inner loop
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (comp.compare(S[j], S[j + 1]) > 0) {
+                    // Swap S[j] and S[j + 1]
+                    K temp = S[j];
+                    S[j] = S[j + 1];
+                    S[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // Early stop if no swaps
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    /**
+     * Standard Insertion Sort.
+     */
+    public static <K> void insertionSort(K[] S, Comparator<K> comp) {
+        int n = S.length;
+
+        for (int i = 1; i < n; i++) {
+            K cur = S[i];
+            int j = i - 1;
+
+            // Shift elements to the right
+            while (j >= 0 && comp.compare(S[j], cur) > 0) {
+                S[j + 1] = S[j];
+                j--;
+            }
+
+            // Insert cur in correct position
+            S[j + 1] = cur;
+        }
+    }
+}
